cfd4174b6bd74092f3b58052e96617d6390ee282
s4chin-verma <shivamvarma346@gmail.com>
2024-02-15 22:58:37 +0530

    issue fixed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,20 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
     },
   },
 });
